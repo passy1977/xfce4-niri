f773e7715c3da0617575ad8b413f138a8e9989ae
Antonio Salsi <antonio.salsi@kiwitron.com>
2026-08-07 15:46:23 +0100

    WIP

--- xfce4-niri-service/src/autostart.rs
+++ xfce4-niri-service/src/autostart.rs
@@ -18,29 +18,31 @@
  *
  ***************************************************************************/
 
+use std::collections::HashMap;
+use std::fs::DirEntry;
+use std::ptr::null_mut;
 use std::sync::Arc;
 
-use osal_rs::os::{Mutex, Thread, ThreadFn, ThreadParam};
-use osal_rs::utils::Result;
+use osal_rs::os::{Mutex, MutexFn, Thread, ThreadFn, ThreadParam};
+use osal_rs::utils::{DoublePtr, Ptr, Result};
 
-use crate::brightness::BrightnessData;
-
+use crate::data::{Data, XDG_AUTOSTART};
 
 pub(crate) struct AutostartData {
-    device: String,
-    value: i32
+    xdg_autostart:  HashMap<String, DirEntry>,
+    local_autostart: HashMap<String, DirEntry>
 }
 
 impl Default for AutostartData {
     fn default() -> Self {
-        Self { device: Default::default(), value: -2 }
+        Self { xdg_autostart: Default::default(), local_autostart: Default::default() }
     }
 }
 
 
 pub(crate) struct Autostart {
     thread: Thread,
-    data: Arc<Mutex<BrightnessData>>,
+    data: Arc<Mutex<AutostartData>>,
 }
 
 impl Autostart {
@@ -57,14 +59,23 @@ impl Autostart {
 
     pub(super) fn start(&mut self) -> Result<()>{
 
+        let mut data = self.data.lock()?;
+
+        data.xdg_autostart = Data::read_directory(XDG_AUTOSTART)?.iter().map(|it| (it.file_name().to_string_lossy().to_string(), it.clone())).collect();
+        data.local_autostart = Data::read_directory(&data.xdg_autostart.values().next().unwrap().path().parent().unwrap().to_string_lossy())?.iter().map(|it| (it.file_name().to_string_lossy().to_string(), it.clone())).collect();
+
         let param: Option<ThreadParam> = Some(self.data.clone());
 
-        self.thread.spawn(param, |_, param| {
+        let thread_local = self.thread.spawn(param, |_, param| {
+
 
     
 
             Ok(Arc::new(()))
         })?;
+
+        let mut ret: Ptr = null_mut();
+        thread_local.join(&raw mut ret)?;
 
         Ok(())
     }
